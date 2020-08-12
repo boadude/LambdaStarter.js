e3652b91737bc1c2f4f5a3537e4e06c94efd717d
Coding & Fitness <boadude@gmail.com>
2020-08-12 06:21:06 -0400

    feat(eslint): se le agrega Eslint al proyecto

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ export const lambdaHandler = async (): Promise<APIGatewayProxyResult> => {
   logger.info(`time to say 'hello'`);
 
   logger.info(`Testomg pme more time`);
-  
+
   return { body: JSON.stringify({ message: 'hello' }), statusCode: 200 };
 }
